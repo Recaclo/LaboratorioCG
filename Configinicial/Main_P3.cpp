c5de25f65c8de5db1a8a803fa8352ba0d0e3b512
Laura Reyes <laurarey1019@gmail.com>
2026-02-27 20:31:31 -0600

    Add 5 figures

--- Configinicial/Main_P3.cpp
+++ Configinicial/Main_P3.cpp
@@ -1,9 +1,8 @@
-//22 / Febrero / 2026
+//2/ Febrero / 2026
 //Reyes Carrillo Laura
 //320015764
-//Practica 3: Proyecciones, transformaciones y Shaders
-// Hacer la misma cantidad de cubos que la cantidad de letras en mi nombre 
-//Previo 3 Proyecciones, transformaciones y Shaders
+//Practica 3: Proyecciones y transformaciones 
+// Hacer la misma cantidad de cubos que la cantidad de letras en mi nombre
 
 #include<iostream>
 
@@ -15,287 +14,203 @@
 #include <glm/gtc/matrix_transform.hpp>
 #include <glm/gtc/type_ptr.hpp>
 
-
 // Shaders
 #include "Shader.h"
 
 const GLint WIDTH = 800, HEIGHT = 600;
 
-
 int main() {
-	glfwInit();
-	//Verificaci�n de compatibilidad 
-	// Set all the required options for GLFW
-	/*glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
-	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
-	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
-	glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);*/
+    glfwInit();
+    //Verificaci�n de compatibilidad
+    // Set all the required options for GLFW
+    /*glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+    glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);*/
 
-	glfwWindowHint(GLFW_RESIZABLE, GL_FALSE);
+    glfwWindowHint(GLFW_RESIZABLE, GL_FALSE);
 
-	GLFWwindow *window = glfwCreateWindow(WIDTH, HEIGHT, "Reyes Carrillo Laura Proyecciones y transformaciones basicas", nullptr, nullptr);
+    GLFWwindow* window = glfwCreateWindow(WIDTH, HEIGHT, "Reyes Carrillo Laura Proyecciones y transformaciones basicas", nullptr, nullptr);
 
-	int screenWidth, screenHeight;
+    int screenWidth, screenHeight;
+    glfwGetFramebufferSize(window, &screenWidth, &screenHeight);
 
-	glfwGetFramebufferSize(window, &screenWidth, &screenHeight);
+    //Verificaci�n de errores de creacion  ventana
+    if (nullptr == window)
+    {
+        std::cout << "Failed to create GLFW window" << std::endl;
+        glfwTerminate();
+        return EXIT_FAILURE;
+    }
 
-	//Verificaci�n de errores de creacion  ventana
-	if (nullptr == window)
-	{
-		std::cout << "Failed to create GLFW window" << std::endl;
-		glfwTerminate();
+    glfwMakeContextCurrent(window);
+    glewExperimental = GL_TRUE;
 
-		return EXIT_FAILURE;
-	}
+    //Verificaci�n de errores de inicializaci�n de glew
+    if (GLEW_OK != glewInit()) {
+        std::cout << "Failed to initialise GLEW" << std::endl;
+        return EXIT_FAILURE;
+    }
 
-	glfwMakeContextCurrent(window);
-	glewExperimental = GL_TRUE;
+    // Define las dimensiones del viewport
+    glViewport(0, 0, screenWidth, screenHeight);
 
-	//Verificaci�n de errores de inicializaci�n de glew
+    // Setup OpenGL options
+    glEnable(GL_DEPTH_TEST);
 
-	if (GLEW_OK != glewInit()) {
-		std::cout << "Failed to initialise GLEW" << std::endl;
-		return EXIT_FAILURE;
-	}
+    // enable alpha support
+    glEnable(GL_BLEND);
+    glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
 
+    // Build and compile our shader program
+    Shader ourShader("Shader/core.vs", "Shader/core.frag");
 
-	// Define las dimensiones del viewport
-	glViewport(0, 0, screenWidth, screenHeight);
+    // use with Perspective Projection
+    float vertices[] = {
+        -0.5f, -0.5f, 0.5f, 1.0f, 0.0f,0.0f,//Front
+         0.5f, -0.5f, 0.5f, 1.0f, 0.0f,0.0f,
+         0.5f,  0.5f, 0.5f, 1.0f, 0.0f,0.0f,
+         0.5f,  0.5f, 0.5f, 1.0f, 0.0f,0.0f,
+        -0.5f,  0.5f, 0.5f, 1.0f, 0.0f,0.0f,
+        -0.5f, -0.5f, 0.5f, 1.0f, 0.0f,0.0f,
 
+        -0.5f, -0.5f,-0.5f, 0.0f, 1.0f,0.0f,//Back
+         0.5f, -0.5f,-0.5f, 0.0f, 1.0f,0.0f,
+         0.5f,  0.5f,-0.5f, 0.0f, 1.0f,0.0f,
+         0.5f,  0.5f,-0.5f, 0.0f, 1.0f,0.0f,
+        -0.5f,  0.5f,-0.5f, 0.0f, 1.0f,0.0f,
+        -0.5f, -0.5f,-0.5f, 0.0f, 1.0f,0.0f,
 
-	// Setup OpenGL options
-	glEnable(GL_DEPTH_TEST);
+         0.5f, -0.5f,  0.5f, 0.0f, 0.0f,1.0f,
+         0.5f, -0.5f, -0.5f, 0.0f, 0.0f,1.0f,
+         0.5f,  0.5f, -0.5f, 0.0f, 0.0f,1.0f,
+         0.5f,  0.5f, -0.5f, 0.0f, 0.0f,1.0f,
+         0.5f,  0.5f,  0.5f, 0.0f, 0.0f,1.0f,
+         0.5f, -0.5f,  0.5f, 0.0f, 0.0f,1.0f,
 
-	// enable alpha support
-	glEnable(GL_BLEND);
-	glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
+        -0.5f,  0.5f,  0.5f, 1.0f, 1.0f,0.0f,
+        -0.5f,  0.5f, -0.5f, 1.0f, 1.0f,0.0f,
+        -0.5f, -0.5f, -0.5f, 1.0f, 1.0f,0.0f,
+        -0.5f, -0.5f, -0.5f, 1.0f, 1.0f,0.0f,
+        -0.5f, -0.5f,  0.5f, 1.0f, 1.0f,0.0f,
+        -0.5f,  0.5f,  0.5f, 1.0f, 1.0f,0.0f,
 
+        -0.5f, -0.5f, -0.5f, 0.0f, 1.0f,1.0f,
+         0.5f, -0.5f, -0.5f, 0.0f, 1.0f,1.0f,
+         0.5f, -0.5f,  0.5f, 0.0f, 1.0f,1.0f,
+         0.5f, -0.5f,  0.5f, 0.0f, 1.0f,1.0f,
+        -0.5f, -0.5f,  0.5f, 0.0f, 1.0f,1.0f,
+        -0.5f, -0.5f, -0.5f, 0.0f, 1.0f,1.0f,
 
-	// Build and compile our shader program
-	Shader ourShader("Shader/core.vs", "Shader/core.frag");
+        -0.5f,  0.5f, -0.5f, 1.0f, 0.2f,0.5f,
+         0.5f,  0.5f, -0.5f, 1.0f, 0.2f,0.5f,
+         0.5f,  0.5f,  0.5f, 1.0f, 0.2f,0.5f,
+         0.5f,  0.5f,  0.5f, 1.0f, 0.2f,0.5f,
+        -0.5f,  0.5f,  0.5f, 1.0f, 0.2f,0.5f,
+        -0.5f,  0.5f, -0.5f, 1.0f, 0.2f,0.5f,
+    };
 
+    GLuint VBO, VAO;
+    glGenVertexArrays(1, &VAO);
+    glGenBuffers(1, &VBO);
 
-	// Set up vertex data (and buffer(s)) and attribute pointers
-	// use with Orthographic Projection
+    // Enlazar  Vertex Array Object
+    glBindVertexArray(VAO);
 
-	//GLfloat vertices[] = {
- //      -0.5f*500, -0.5f, 0.5f, 1.0f, 0.0f,0.0f,//Front
-	//	0.5f * 500, -0.5f * 500, 0.5f * 500,  1.0f, 0.0f,0.0f,
-	//	0.5f * 500,  0.5f * 500, 0.5f * 500,  1.0f, 0.0f,0.0f,
-	//	0.5f * 500,  0.5f * 500, 0.5f * 500,  1.0f, 0.0f,0.0f,
-	//	-0.5f * 500,  0.5f * 500, 0.5f * 500, 1.0f, 0.0f,0.0f,
-	//	-0.5f * 500, -0.5f * 500, 0.5f * 500, 1.0f, 0.0f,0.0f,
-	//	
-	//    -0.5f * 500, -0.5f * 500,-0.5f * 500, 0.0f, 1.0f,0.0f,//Back
-	//	 0.5f * 500, -0.5f * 500,-0.5f * 500, 0.0f, 1.0f,0.0f,
-	//	 0.5f * 500,  0.5f * 500,-0.5f * 500, 0.0f, 1.0f,0.0f,
-	//	 0.5f * 500,  0.5f * 500,-0.5f * 500, 0.0f, 1.0f,0.0f,
-	//    -0.5f * 500,  0.5f * 500,-0.5f * 500, 0.0f, 1.0f,0.0f,
-	//    -0.5f * 500, -0.5f * 500,-0.5f * 500, 0.0f, 1.0f,0.0f,
-	//	
-	//	 0.5f * 500, -0.5f * 500,  0.5f * 500,  0.0f, 0.0f,1.0f,
-	//	 0.5f * 500, -0.5f * 500, -0.5f * 500,  0.0f, 0.0f,1.0f,
-	//	 0.5f * 500,  0.5f * 500, -0.5f * 500,  0.0f, 0.0f,1.0f,
-	//	 0.5f * 500,  0.5f * 500, -0.5f * 500,  0.0f, 0.0f,1.0f,
-	//	 0.5f * 500,  0.5f * 500,  0.5f * 500,  0.0f, 0.0f,1.0f,
-	//	 0.5f * 500,  -0.5f * 500, 0.5f * 500, 0.0f, 0.0f,1.0f,
- //     
-	//	-0.5f * 500,  0.5f * 500,  0.5f * 500,  1.0f, 1.0f,0.0f,
-	//	-0.5f * 500,  0.5f * 500, -0.5f * 500,  1.0f, 1.0f,0.0f,
-	//	-0.5f * 500, -0.5f * 500, -0.5f * 500,  1.0f, 1.0f,0.0f,
-	//	-0.5f * 500, -0.5f * 500, -0.5f * 500,  1.0f, 1.0f,0.0f,
-	//	-0.5f * 500, -0.5f * 500,  0.5f * 500,  1.0f, 1.0f,0.0f,
-	//	-0.5f * 500,  0.5f * 500,  0.5f * 500,  1.0f, 1.0f,0.0f,
-	//	
-	//	-0.5f * 500, -0.5f * 500, -0.5f * 500, 0.0f, 1.0f,1.0f,
-	//	0.5f * 500, -0.5f * 500, -0.5f * 500,  0.0f, 1.0f,1.0f,
-	//	0.5f * 500, -0.5f * 500,  0.5f * 500,  0.0f, 1.0f,1.0f,
-	//	0.5f * 500, -0.5f * 500,  0.5f * 500,  0.0f, 1.0f,1.0f,
-	//	-0.5f * 500, -0.5f * 500,  0.5f * 500, 0.0f, 1.0f,1.0f,
-	//	-0.5f * 500, -0.5f * 500, -0.5f * 500, 0.0f, 1.0f,1.0f,
-	//	
-	//	-0.5f * 500,  0.5f * 500, -0.5f * 500, 1.0f, 0.2f,0.5f,
-	//	0.5f * 500,  0.5f * 500, -0.5f * 500,  1.0f, 0.2f,0.5f,
-	//	0.5f * 500,  0.5f * 500,  0.5f * 500,  1.0f, 0.2f,0.5f,
-	//	0.5f * 500,  0.5f * 500,  0.5f * 500,  1.0f, 0.2f,0.5f,
-	//	-0.5f * 500,  0.5f * 500,  0.5f * 500, 1.0f, 0.2f,0.5f,
-	//	-0.5f * 500,  0.5f * 500, -0.5f * 500, 1.0f, 0.2f,0.5f,
-	//};
-	
+    //2.- Copiamos nuestros arreglo de vertices en un buffer de vertices para que OpenGL lo use
+    glBindBuffer(GL_ARRAY_BUFFER, VBO);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
 
-	// use with Perspective Projection
-	float vertices[] = {
-		-0.5f, -0.5f, 0.5f, 1.0f, 0.0f,0.0f,//Front
-		0.5f, -0.5f, 0.5f,  1.0f, 0.0f,0.0f,
-		0.5f,  0.5f, 0.5f,  1.0f, 0.0f,0.0f,
-		0.5f,  0.5f, 0.5f,  1.0f, 0.0f,0.0f,
-		-0.5f,  0.5f, 0.5f, 1.0f, 0.0f,0.0f,
-		-0.5f, -0.5f, 0.5f, 1.0f, 0.0f,0.0f,
-		
-	    -0.5f, -0.5f,-0.5f, 0.0f, 1.0f,0.0f,//Back
-		 0.5f, -0.5f,-0.5f, 0.0f, 1.0f,0.0f,
-		 0.5f,  0.5f,-0.5f, 0.0f, 1.0f,0.0f,
-		 0.5f,  0.5f,-0.5f, 0.0f, 1.0f,0.0f,
-	    -0.5f,  0.5f,-0.5f, 0.0f, 1.0f,0.0f,
-	    -0.5f, -0.5f,-0.5f, 0.0f, 1.0f,0.0f,
-		
-		 0.5f, -0.5f,  0.5f,  0.0f, 0.0f,1.0f,
-		 0.5f, -0.5f, -0.5f,  0.0f, 0.0f,1.0f,
-		 0.5f,  0.5f, -0.5f,  0.0f, 0.0f,1.0f,
-		 0.5f,  0.5f, -0.5f,  0.0f, 0.0f,1.0f,
-		 0.5f,  0.5f,  0.5f,  0.0f, 0.0f,1.0f,
-		 0.5f,  -0.5f, 0.5f, 0.0f, 0.0f,1.0f,
-      
-		-0.5f,  0.5f,  0.5f,  1.0f, 1.0f,0.0f,
-		-0.5f,  0.5f, -0.5f,  1.0f, 1.0f,0.0f,
-		-0.5f, -0.5f, -0.5f,  1.0f, 1.0f,0.0f,
-		-0.5f, -0.5f, -0.5f,  1.0f, 1.0f,0.0f,
-		-0.5f, -0.5f,  0.5f,  1.0f, 1.0f,0.0f,
-		-0.5f,  0.5f,  0.5f,  1.0f, 1.0f,0.0f,
-		
-		-0.5f, -0.5f, -0.5f, 0.0f, 1.0f,1.0f,
-		0.5f, -0.5f, -0.5f,  0.0f, 1.0f,1.0f,
-		0.5f, -0.5f,  0.5f,  0.0f, 1.0f,1.0f,
-		0.5f, -0.5f,  0.5f,  0.0f, 1.0f,1.0f,
-		-0.5f, -0.5f,  0.5f, 0.0f, 1.0f,1.0f,
-		-0.5f, -0.5f, -0.5f, 0.0f, 1.0f,1.0f,
-		
-		-0.5f,  0.5f, -0.5f, 1.0f, 0.2f,0.5f,
-		0.5f,  0.5f, -0.5f,  1.0f, 0.2f,0.5f,
-		0.5f,  0.5f,  0.5f,  1.0f, 0.2f,0.5f,
-		0.5f,  0.5f,  0.5f,  1.0f, 0.2f,0.5f,
-		-0.5f,  0.5f,  0.5f, 1.0f, 0.2f,0.5f,
-		-0.5f,  0.5f, -0.5f, 1.0f, 0.2f,0.5f,
-	};
+    // 4. Despues colocamos las caracteristicas de los vertices
 
+    //Posicion
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(GLfloat), (GLvoid*)0);
+    glEnableVertexAttribArray(0);
 
+    //Color
+    glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(GLfloat), (GLvoid*)(3 * sizeof(GLfloat)));
+    glEnableVertexAttribArray(1);
 
+    glBindBuffer(GL_ARRAY_BUFFER, 0);
+    glBindVertexArray(0); // Unbind VAO
 
-	GLuint VBO, VAO;
-	glGenVertexArrays(1, &VAO);
-	glGenBuffers(1, &VBO);
-	//glGenBuffers(1, &EBO);
+    glm::mat4 projection = glm::mat4(1.0f);
+    //definimos proyecciones
+    // IMPORTANTE: glm::perspective usa radianes, por eso usamos glm::radians(45.0f)
+    projection = glm::perspective(glm::radians(45.0f),
+        (GLfloat)screenWidth / (GLfloat)screenHeight,
+        0.1f, 100.0f);
 
-	// Enlazar  Vertex Array Object
-	glBindVertexArray(VAO);
+    // POSICIONES DE LOS 5 CUBOS (centrados y simetricos)
+    // Separacion uniforme para que se vean "bonitos"
+    glm::vec3 cubePositions[5] = {
+        glm::vec3(-4.0f, 0.0f, 0.0f),
+        glm::vec3(-2.0f, 0.0f, 0.0f),
+        glm::vec3(0.0f, 0.0f, 0.0f),
+        glm::vec3(2.0f, 0.0f, 0.0f),
+        glm::vec3(4.0f, 0.0f, 0.0f)
+    };
 
-	//2.- Copiamos nuestros arreglo de vertices en un buffer de vertices para que OpenGL lo use
-	glBindBuffer(GL_ARRAY_BUFFER, VBO);
-	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
-	// 3.Copiamos nuestro arreglo de indices en  un elemento del buffer para que OpenGL lo use
-	/*glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
-	glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);*/
+    while (!glfwWindowShouldClose(window))
+    {
+        // Check if any events have been activiated (key pressed, mouse moved etc.) and call corresponding response functions
+        glfwPollEvents();
 
-	// 4. Despues colocamos las caracteristicas de los vertices
+        // Render
+        // Clear the colorbuffer
+        glClearColor(0.95f, 0.95f, 0.95f, 1.0f); // fondo gris claro para que se vea mas "bonito"
+        glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
-	//Posicion
-	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(GLfloat), (GLvoid *)0);
-	glEnableVertexAttribArray(0);
+        // mandamos la inofrmacion a la parte del shader
+        ourShader.Use();
 
-	//Color
-	glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(GLfloat), (GLvoid *)(3 * sizeof(GLfloat)));
-	glEnableVertexAttribArray(1);
+        // Se crean matrices view (camara) y se manda projection una vez
+        glm::mat4 view = glm::mat4(1.0f);
 
-	glBindBuffer(GL_ARRAY_BUFFER, 0);
+        // Colocamos la camara un poco alejada para ver los 5 cubos completos
+        view = glm::translate(view, glm::vec3(0.0f, 0.0f, -10.0f));
 
+        // comunicacion por medio de las variables uniform
+        GLint modelLoc = glGetUniformLocation(ourShader.Program, "model");
+        GLint viewLoc = glGetUniformLocation(ourShader.Program, "view");
+        GLint projecLoc = glGetUniformLocation(ourShader.Program, "projection");
 
-	glBindVertexArray(0); // Unbind VAO (it's always a good thing to unbind any buffer/array to prevent strange bugs)
+        // Enviamos projection y view UNA SOLA VEZ por frame
+        glUniformMatrix4fv(projecLoc, 1, GL_FALSE, glm::value_ptr(projection));
+        glUniformMatrix4fv(viewLoc, 1, GL_FALSE, glm::value_ptr(view));
 
-	
-	glm::mat4 projection=glm::mat4(1);
-	//creamos una matriz que iniciamos con valores de 1 
+        glBindVertexArray(VAO);
 
-	//definimos proyecciones 
-	projection = glm::perspective(45.0f, (GLfloat)screenWidth / (GLfloat)screenHeight, 0.1f, 100.0f);//FOV, Radio de aspecto,znear,zfar
-	//projection = glm::ortho(0.0f, (GLfloat)screenWidth, 0.0f, (GLfloat)screenHeight, 0.1f, 1000.0f);//Izq,Der,Fondo,Alto,Cercania,Lejania
-	// se le asigna la funcion para crear la proyeccion 
-	// limites, primero es el izquierdo que va del 0, limite de la ventana 
-	//limite inferior y limite superior corresponde a lo alto de la pantalla 
-	//  0.1f, 1000.0f planos o distancias: desde donde vamos a comenzar a ver y hasta donde vamos a alcanar a ver 
-	while (!glfwWindowShouldClose(window))
-	{
-		// Check if any events have been activiated (key pressed, mouse moved etc.) and call corresponding response functions
-		glfwPollEvents();
+        // Dibujamos 5 cubos estaticos 
+        for (int i = 0; i < 5; i++)
+        {
+            // Matriz model para cada cubo
+            glm::mat4 model = glm::mat4(1.0f);
 
-		// Render
-		// Clear the colorbuffer
-		glClearColor(1.0f, 1.0f, 1.0f, 1.0f);
-		glClear(GL_COLOR_BUFFER_BIT| GL_DEPTH_BUFFER_BIT);
+            // 1) Mover cada cubo a su posicion
+            model = glm::translate(model, cubePositions[i]);
 
-		// Draw our first triangle
-		// mandamos la inofrmacion a la parte del shader 
-		ourShader.Use();
-		// se crean 2 matrices model y view 
-		//Cubo 1 
-		glm::mat4 model=glm::mat4(1);
-		glm::mat4 view=glm::mat4(1);
-	
-		view = glm::translate(view, glm::vec3(0.0f,0.0f,-12.0f));
-		model = glm::rotate( model, 0.5f, glm::vec3( 0.0f, 1.0f, 0.0f ) ); // use to compare orthographic and perspective projection
-		model = glm::scale(model, glm::vec3(2.0f, 3.0f, 1.0f));
-		//view = glm::translate( view, glm::vec3( screenWidth / 2, screenHeight / 1,-800.0f ) ); // use with orthographic projection
-		// se manda la infor al shader y el lo interpreta y lo dibuja 
-		//comunicacion por medio de las variables uniform
-		//variable de tipo GLint 
-		GLint modelLoc = glGetUniformLocation(ourShader.Program, "model");
-		GLint viewLoc = glGetUniformLocation(ourShader.Program, "view");
-		GLint projecLoc = glGetUniformLocation(ourShader.Program, "projection");
-		
-		glUniformMatrix4fv(projecLoc, 1, GL_FALSE, glm::value_ptr(projection));
-		glUniformMatrix4fv(viewLoc, 1, GL_FALSE, glm::value_ptr(view));
-		glUniformMatrix4fv(modelLoc, 1, GL_FALSE, glm::value_ptr(model));
+            // 2) Pequeña inclinacion fija para que se vea 3D (NO es animacion)
+            model = glm::rotate(model, glm::radians(25.0f), glm::vec3(0.0f, 1.0f, 0.0f));
+            model = glm::rotate(model, glm::radians(12.0f), glm::vec3(1.0f, 0.0f, 0.0f));
 
-		glBindVertexArray(VAO);
-		glDrawArrays(GL_TRIANGLES, 0, 36);
-		// para crear otro elemento hacemos lo siguiente :
-		// creamos una matriz 
-		//Cubo 2
-		model = glm::mat4(1);
-		model = glm::translate(model, glm::vec3(5.0f, 0.0f, 0.0f));
-		model = glm::rotate(model, 45.0f, glm::vec3(0.0f, 0.0f, 1.0f)); // use to compare orthographic and perspective projection
-		model = glm::scale(model, glm::vec3(8.0f, 1.0f, 5.0f));
-		glUniformMatrix4fv(modelLoc, 1, GL_FALSE, glm::value_ptr(model)); 
-		glDrawArrays(GL_TRIANGLES, 0, 36);
-		//Cubo 3
-		model = glm::mat4(1); 
-		model = glm::translate(model, glm::vec3(-5.0f, 0.0f, 0.0f));
-		model = glm::rotate(model, 45.0f, glm::vec3(1.0f, 0.0f, 0.0f)); // use to compare orthographic and perspective projection
-		model = glm::scale(model, glm::vec3(8.0f, 1.0f, 5.0f));
-		glUniformMatrix4fv(modelLoc, 1, GL_FALSE, glm::value_ptr(model));
-		glDrawArrays(GL_TRIANGLES, 0, 36);
-		//Cubo 4 
-		model = glm::mat4(1);
-		model = glm::translate(model, glm::vec3(0.0f, 3.0f, 0.0f));
-		model = glm::rotate(model, 45.0f, glm::vec3(0.0f, 1.0f, 0.0f)); // use to compare orthographic and perspective projection
-		model = glm::scale(model, glm::vec3(8.0f, 1.0f, 5.0f));
-		glUniformMatrix4fv(modelLoc, 1, GL_FALSE, glm::value_ptr(model));
-		glDrawArrays(GL_TRIANGLES, 0, 36);
-		//Cubo 5
-		model = glm::mat4(1);
-		model = glm::translate(model, glm::vec3(0.0f, -3.0f, 0.0f));
-		model = glm::rotate(model, 45.0f, glm::vec3(0.0f, 0.0f, 1.0f)); // use to compare orthographic and perspective projection
-		model = glm::scale(model, glm::vec3(8.0f, 1.0f, 5.0f));
-		glUniformMatrix4fv(modelLoc, 1, GL_FALSE, glm::value_ptr(model));
-		glDrawArrays(GL_TRIANGLES, 0, 36);
+            // 3) Escala uniforme para que todos sean simetricos (no deformados)
+            model = glm::scale(model, glm::vec3(1.0f));
 
+            // Mandamos la matriz model y dibujamos el cubo
+            glUniformMatrix4fv(modelLoc, 1, GL_FALSE, glm::value_ptr(model));
+            glDrawArrays(GL_TRIANGLES, 0, 36);
+        }
 
-		glBindVertexArray(0); 
-		
-		
+        glBindVertexArray(0);
 
-		// Swap the screen buffers
-		glfwSwapBuffers(window);
-	
-	}
-	glDeleteVertexArrays(1, &VAO);
-	glDeleteBuffers(1, &VBO);
+        // Swap the screen buffers
+        glfwSwapBuffers(window);
+    }
 
+    glDeleteVertexArrays(1, &VAO);
+    glDeleteBuffers(1, &VBO);
 
-	glfwTerminate();
-	return EXIT_SUCCESS;
-
-  
-
+    glfwTerminate();
+    return EXIT_SUCCESS;
 }
-
-
